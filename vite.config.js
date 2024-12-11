86a4b0dace4f43cab7d22629aff4b16ad340d1cc
marugandev <marugandev@gmail.com>
2024-12-11 12:21:33 +0100

    DELETE vercel.json and FEAT vite.config.js yo disable minify

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    minify: false // Desactivar minificación para evitar que se eliminen clases dinámicas
+  }
+});
