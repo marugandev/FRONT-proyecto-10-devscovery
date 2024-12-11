4a8dec8b4de796bfad368928b15a8f1142055cac
marugandev <marugandev@gmail.com>
2024-12-11 17:09:25 +0100

    FEAT .env and .end.production and API BASE_URL

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -1,5 +1,13 @@
-const URL = "http://localhost:3000/api/v1";
+/* const URL = "http://localhost:3000/api/v1"; */
 // development URL
+
+/* const BASE_URL =
+  process.env.VITE_API_URL === "production"
+    ? "https://front-proyecto-10-devscovery-rtc.vercel.app/" // URL de producción
+    : "http://localhost:3000"; */
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+console.log("Base URL:", BASE_URL);
 
 export const API = async ({
   endpoint,
@@ -16,7 +24,7 @@ export const API = async ({
 
   const combinedHeaders = { ...defaultHeaders, ...headers };
 
-  const res = await fetch(URL + endpoint, {
+  const res = await fetch(BASE_URL + endpoint, {
     headers: combinedHeaders,
     body: isJSON ? JSON.stringify(body) : body,
     method
