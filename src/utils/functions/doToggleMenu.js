bbd46460a089b56943091f6d62dc0fd9177e0b3f
marugandev <marugandev@gmail.com>
2024-12-11 10:48:06 +0100

    FIX relative routes doToggleMenu icon toggle

--- src/utils/functions/doToggleMenu.js
+++ src/utils/functions/doToggleMenu.js
@@ -5,16 +5,16 @@ export const doToggleMenu = () => {
     menu.classList.toggle("header__menu-toggle");
 
     const iconToggleMenu = document.querySelector(
-      'img[src="/public/assets/icons/devscovery_menu_close.svg"], img[src="/public/assets/icons/devscovery_menu_open.svg"]'
+      'img[src="/assets/icons/devscovery_menu_close.svg"], img[src="/assets/icons/devscovery_menu_open.svg"]'
     );
 
     if (iconToggleMenu) {
       if (iconToggleMenu.src.includes("menu_close")) {
-        iconToggleMenu.src = "/public/assets/icons/devscovery_menu_open.svg";
+        iconToggleMenu.src = "/assets/icons/devscovery_menu_open.svg";
         iconToggleMenu.alt = "Abrir menú";
         iconToggleMenu.title = "Abrir menú";
       } else {
-        iconToggleMenu.src = "/public/assets/icons/devscovery_menu_close.svg";
+        iconToggleMenu.src = "/assets/icons/devscovery_menu_close.svg";
         iconToggleMenu.alt = "Cerrar menú";
         iconToggleMenu.title = "Cerrar menú";
       }
