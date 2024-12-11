161c221daa7b7ce650a70210882bda94ced218fd
marugandev <marugandev@gmail.com>
2024-12-11 22:26:56 +0100

    DELETE vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  build: {
-    minify: false, // Desactivar minificación para evitar que se eliminen clases dinámicas
-    cssCodeSplit: false
-  }
-});
