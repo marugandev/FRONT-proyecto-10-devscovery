edc46f70bbab85e2e612b4df3fe17f2e9602c110
marugandev <marugandev@gmail.com>
2025-01-08 14:07:11 +0100

    UPDATE pages MyEvents alerts

--- src/pages/MyEvents/MyEvents.js
+++ src/pages/MyEvents/MyEvents.js
@@ -3,6 +3,10 @@ import { CreatePage } from "../../utils/functions/createPage";
 import { RenderEvent } from "../../components/RenderEvent/RenderEvent";
 import { getUserById } from "../../utils/functions/userService";
 import { Events } from "../Events/Events";
+import {
+  Alert,
+  RemoveAlert
+} from "../../components/AlertManagement/AlertManagement";
 
 export const MyEvents = async () => {
   const section = CreatePage({
@@ -17,9 +21,15 @@ export const MyEvents = async () => {
     const res = await getUserById(user._id, token);
 
     if (res.data.favoriteEvents.length === 0) {
-      alert("No tienes eventos reservados en este momento");
-      window.history.pushState("", "", "/events");
-      Events();
+      const onAccept = () => {
+        RemoveAlert();
+        window.history.pushState("", "", "/events");
+        Events();
+      };
+      Alert({
+        textContent: "No tienes eventos reservados en este momento",
+        onAccept
+      });
     } else {
       RenderEvent({
         parentElement: section,
@@ -27,10 +37,12 @@ export const MyEvents = async () => {
       });
     }
   } catch (error) {
+    Alert({
+      textContent:
+        "Error al obtener tus eventos. Por favor, intentalo de nuevo más tarde.",
+      onAccept: RemoveAlert
+    });
     console.error("Error al obtener los eventos del usuario:", error);
-    alert(
-      "Error al obtener tus eventos. Por favor, intentalo de nuevo más tarde."
-    );
   }
   return section;
 };
